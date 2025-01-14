30996ce17f1ac03862e49f46fd750fb9c81301d9
Eddort <webisforce@gmail.com>
2025-01-14 15:00:22 +0100

    feat: save keys with original permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 # Execute the basic command
 python3 ./staking_deposit/deposit.py "$@"
 
+mkdir /app/validator_keys_docker
+
+cp -r /app/validator_keys /app/validator_keys_docker
+
 # Execute the chown command after the main command
 chown -R $UID:$GID /app/validator_keys
