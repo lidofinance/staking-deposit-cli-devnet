033f160cf2ffca057e9db647523782190024cf40
Eddort <webisforce@gmail.com>
2025-01-14 18:33:34 +0100

    feat: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 # Execute the basic command
 python3 ./staking_deposit/deposit.py "$@"
 
-mkdir /app/validator_keys_docker
+# mkdir /app/validator_keys_docker
 
-cp -r /app/validator_keys /app/validator_keys_docker
+# cp -rf /app/validator_keys/ /app/validator_keys_docker/
 
 # Execute the chown command after the main command
-chown -R $UID:$GID /app/validator_keys
+# chown -R $UID:$GID /app/validator_keys
+
+chmod -R 777 /app/validator_keys
